7b4fbdba0d7638804ff5b8c8bf0f29173d9eee86
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-10-14 07:11:41 +0700

    Методы для вывода инфы и родителей

--- FamilyTree/FamilyTree/FamilyMember.cs
+++ FamilyTree/FamilyTree/FamilyMember.cs
@@ -6,38 +6,34 @@ using System.Threading.Tasks;
 
 namespace FamilyTree;
 
-    public class FamilyMember
+public class FamilyMember
+{
+    private string _name;
+    private DateTime _birthday;
+    private FamilyMember _father;
+    private FamilyMember _mother;
+    private Gender _gender;
+    private List<FamilyMember> _members;
+    private FamilyMember _wife;
+    private FamilyMember _husband;
+
+    public FamilyMember(string name, DateTime birthday, Gender gender)
     {
-        private string _name;
-        private DateTime _birthday;
-        private FamilyMember _father;
-        private FamilyMember _mother;
-        private Gender _gender;
-        private List<FamilyMember> _members;
-
-        
-
-
-
-        public FamilyMember(string name, DateTime birthday,Gender gender)
-        {
-            this.Name = name;
-            this.Gender1 = gender;
-            Birthday = birthday;
-            _members = new List<FamilyMember>();
-        }
+        Name = name;
+        this.Gender1 = gender;
+        Birthday = birthday;
+        _members = new List<FamilyMember>();
+    }
 
     #region Свойства_полей
+
     public string Name
     {
         get => _name;
         set => _name = value;
     }
 
-    public DateTime Birthday
-    {
-        get => _birthday;
-    }
+    public DateTime Birthday => _birthday;
 
     public FamilyMember Father
     {
@@ -56,13 +52,34 @@ namespace FamilyTree;
         get => _gender;
         set => _gender = value;
     }
+
+    public FamilyMember Wife
+    {
+        get => _wife;
+        set => _wife = value;
+    }
+
+    public FamilyMember Husband
+    {
+        get => _husband;
+        set => _husband = value;
+    }
+
     #endregion
 
     public override string ToString()
-        {
-            return $"Имя = {Name}" +
-                   $" \n Пол = {Gender1}\n" +
-                   $"Дата рождения = {_birthday}";
-        }
+    {
+        return $"Name = {_name} Пол = {_gender} Birthday = {_birthday}";
     }
 
+    string PrintInfo()
+    {
+        return $"{this}";
+    }
+
+    string PrintParents()
+    {
+        return $"Father = {_father} \n" +
+               $"Moter = {_mother}";
+    }
+}
