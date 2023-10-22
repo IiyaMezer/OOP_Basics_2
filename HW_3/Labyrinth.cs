789426c59c7812e3d4ee89faae4e95de3b17fc54
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-10-22 19:17:02 +0700

    Initial creation

---
+++ HW_3/Labyrinth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Labyrinth
+/// </summary>
+public class Labyrinth
+{
+	public bool HasExit(int[,] labyrinth, int startI, int startJ)
+	{
+		if (labyrinth[startI, startJ] == 1)
+		{
+			Console.WriteLine("Вы попали в стену.");
+			return false;
+		}
+		else if (labyrinth[startI,startJ] ==2)
+		{
+			Console.WriteLine("Вход является выходом.");
+			return true;
+		}
+		
+	}
+}
---
+++ HW_3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HW_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] labirynth1 = new int[,]
+            {
+                {1, 1, 1, 1, 1, 1, 1 },
+                {1, 0, 0, 0, 0, 0, 1 },
+                {1, 0, 1, 1, 1, 0, 1 },
+                {0, 0, 0, 0, 1, 0, 2 },
+                {1, 1, 0, 0, 1, 1, 1 },
+                {1, 1, 1, 0, 1, 1, 1 },
+                {1, 1, 1, 1, 1, 1, 1 }
+            };
+        }
+    }
+}
