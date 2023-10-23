161376998e5643897d20f1bbde13edcad27df4ea
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-10-23 21:25:07 +0700

    1

--- FamilyTree/HW_3/Program.cs
+++ FamilyTree/HW_3/Program.cs
@@ -6,16 +6,16 @@ internal class Program
     {
         int[,] labirynth1 = new int[,]
             {
-            {1, 1, 1, 1, 1, 1, 1 },
+            {1, 0, 1, 1, 1, 1, 1 },
             {1, 0, 0, 0, 0, 0, 1 },
             {1, 0, 1, 1, 1, 0, 1 },
-            {0, 0, 0, 0, 1, 0, 2 },
+            {1, 0, 0, 0, 1, 0, 2 },
             {1, 1, 0, 0, 1, 1, 1 },
             {1, 1, 1, 0, 1, 1, 1 },
             {1, 1, 1, 1, 1, 1, 1 }
             };
         Console.WriteLine(labirynth1[3, 0]);
-        new Labyrinth().HasExit(labirynth1, 3, 0);
+        new Labyrinth().HasExit(labirynth1, 0, 1);
 
         
 
