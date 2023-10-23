cadf40bd78f40c4239ea1dbd2fa18b4a10660994
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-10-23 21:14:40 +0700

    рефакторинг для читаемости

--- FamilyTree/HW_3/Labyrinth.cs
+++ FamilyTree/HW_3/Labyrinth.cs
@@ -12,18 +12,18 @@ public class Labyrinth
     /// <summary>
     /// Проверка возможности выйти из лабиринта
     /// </summary>
-    /// <param name="labyrinth">исследуемый лабиринт</param>
-    /// <param name="startI">стартовый столбец</param>
-    /// <param name="startJ">стартовая строка</param>
+    /// <param name="l">исследуемый лабиринт</param>
+    /// <param name="startI">стартовая строка</param>
+    /// <param name="startJ">стартовый столбец</param>
     /// <returns>true/false</returns>
-    public bool HasExit(int[,] labyrinth, int startI, int startJ)
+    public bool HasExit(int[,] l, int startI, int startJ)
     {
-        if (labyrinth[startI, startJ] == 1)
+        if (l[startI, startJ] == 1)
         {
             Console.WriteLine("Вы попали в стену.");
             return false;
         }
-        else if (labyrinth[startI, startJ] == 2)
+        else if (l[startI, startJ] == 2)
         {
 
             Console.WriteLine("Вход является выходом.");
@@ -37,23 +37,23 @@ public class Labyrinth
         {
             var temp = stack.Pop();
 
-            if (labyrinth[temp.Item1, temp.Item2] == 2)
+            if (l[temp.Item1, temp.Item2] == 2)
             {
                 Console.WriteLine("Found!");
                 return true;
             }
-            labyrinth[temp.Item1, temp.Item2] = 1;
+            l[temp.Item1, temp.Item2] = 1;
 
-            if (temp.Item2 >= 0 && labyrinth[temp.Item1, temp.Item2 - 1] != 1)
+            if (temp.Item2 >= 0 && l[temp.Item1, temp.Item2 - 1] != 1)
                 stack.Push(new(temp.Item1, temp.Item2 - 1)); //up
 
-            if (temp.Item2 + 1 < labyrinth.GetLength(1) && labyrinth[temp.Item1, temp.Item2 + 1] != 1)
+            if (temp.Item2 + 1 < l.GetLength(1) && l[temp.Item1, temp.Item2 + 1] != 1)
                 stack.Push(new(temp.Item1, temp.Item2 + 1)); //down
 
-            if (temp.Item1 >= 0 && labyrinth[temp.Item1 - 1, temp.Item2] != 1)
+            if (temp.Item1 >= 0 && l[temp.Item1 - 1, temp.Item2] != 1)
                 stack.Push(new(temp.Item1 - 1, temp.Item2)); //left
 
-            if (temp.Item1 + 1 < labyrinth.GetLength(0) && labyrinth[temp.Item1 + 1, temp.Item2] != 1)
+            if (temp.Item1 + 1 < l.GetLength(0) && l[temp.Item1 + 1, temp.Item2] != 1)
                 stack.Push(new(temp.Item1 + 1, temp.Item2)); //rigth
         }
 
--- FamilyTree/HW_3/Program.cs
+++ FamilyTree/HW_3/Program.cs
@@ -14,7 +14,10 @@ internal class Program
             {1, 1, 1, 0, 1, 1, 1 },
             {1, 1, 1, 1, 1, 1, 1 }
             };
+        Console.WriteLine(labirynth1[3, 0]);
         new Labyrinth().HasExit(labirynth1, 3, 0);
+
+        
 
         Console.ReadKey();
         
