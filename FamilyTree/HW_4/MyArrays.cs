5f7a46a7a0b79007f2c68f1ee0351966f109e44a
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-10-29 13:54:48 +0700

    array(List<>)generaton

---
+++ FamilyTree/HW_4/MyArrays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4;
+
+public class MyArrays
+{
+    /// <summary>
+    /// Генерация списка рандомных целых чисел
+    /// </summary>
+    /// <param name="length">длина списка</param>
+    /// <param name="minValue">минимальное значения диапазона генерации</param>
+    /// <param name="maxValue">максимальное значение диапазона генерации</param>
+    /// <returns>список рандомных целых чисел заданной длины в установленном диапазоне</returns>
+    public static List<int> GenerateRandomIntArray(int length, int minValue, int maxValue)
+    {
+        Random random = new Random();
+
+        List<int> randomArray = new List<int>();
+
+        for (int i = 0; i < length; i++)
+        {
+            int number = random.Next(minValue, maxValue + 1);
+            randomArray.Add(number);
+        }
+        return randomArray;
+
+    }
+
+    public static 
+
+
+
+}
--- FamilyTree/HW_4/Program.cs
+++ FamilyTree/HW_4/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace HW_4
+﻿using System.Globalization;
+
+namespace HW_4;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        List<int> nums = MyArrays.GenerateRandomIntArray(10, 1, 15);     
+
+        Console.WriteLine("initial Array: \n" + string.Join(", ", nums.Select(x => x.ToString())));
+
+        Array.Sort(nums);
+
+        Console.WriteLine("sorted Array: \n" + string.Join(", ", nums.Select(x => x.ToString())));
+
+        int targetSum = 10;
+
+        var result = 
+
+
     }
 }
