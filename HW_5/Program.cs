a33a2cb245c1f63fbe731a3694b002901bcd6043
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-11-08 20:03:52 +0700

    работает

--- HW_5/Program.cs
+++ HW_5/Program.cs
@@ -8,19 +8,26 @@ internal class Program
         calc.Result = 0D;
         calc.MyEventHandler += Calc_MyEventHandler;
         
-                string command = String.Empty;
+        string command = String.Empty;
         string operation = String.Empty;
         int num = 0;
 
-        while (true)
+        bool f = true;
+        while (f)
         {
-            
             num = calc.DataInput();
-
             calc.ExecuteCommand(num);
+            //Console.WriteLine("Type 'exit' to quit");
+            //string exit = Console.ReadLine();
+            //if (exit == "exit") 
+            //{
+            //    Console.WriteLine("Press any key to quit");
+            //    f = false;
+            //    Console.ReadKey();
+            //}
         }
 
-        
+
 
 
     }
