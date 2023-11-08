531d2dcf860b6756312c526ed8f5a16a111a1e69
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-11-08 19:50:17 +0700

    калькулятор есть

--- HW_5/Program.cs
+++ HW_5/Program.cs
@@ -5,6 +5,23 @@ internal class Program
     static void Main(string[] args)
     {
         var calc = new Calc();
+        calc.Result = 0D;
+        calc.MyEventHandler += Calc_MyEventHandler;
+        
+                string command = String.Empty;
+        string operation = String.Empty;
+        int num = 0;
+
+        while (true)
+        {
+            
+            num = calc.DataInput();
+
+            calc.ExecuteCommand(num);
+        }
+
+        
+
 
     }
 
