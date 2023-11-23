301eba7673baee7282719e8f9729a414a2c78f52
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-11-23 20:27:58 +0700

    Initial + TestClass

---
+++ FamilyTree/HW_7/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_7
+{
+    internal class TestClass
+    {
+        public int I { get; set; }
+        private string? S { get; set; }
+        public decimal D { get; set; }
+        public char[]? C { get; set; }
+
+        public TestClass()
+        { }
+        public TestClass(int i)
+        {
+            this.I = i;
+        }
+        public TestClass(int i, string s, decimal d, char[] c) : this(i)
+        {
+            this.S = s;
+            this.D = d;
+            this.C = c;
+
+        }
