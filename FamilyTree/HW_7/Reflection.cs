f8e94716cfcba0bb2df3c943cad385cde5bc1db5
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-11-23 20:53:12 +0700

    custom attribute usage

---
+++ FamilyTree/HW_7/Reflection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_7;
+
+public static class Reflection
+{
+    public static string ObjectToString(object obj)
+    {
+        StringBuilder sb = new StringBuilder();
+        Type type = obj.GetType();
+
+        foreach (FieldInfo fieldInfo in type.GetFields())
+
+        {
+
+            MyCustomAttribute attribute = fieldInfo.GetCustomAttribute<MyCustomAttribute>();
+
+
+
+            if (attribute != null)
+
+            {
+
+                string fieldName = attribute.FieldName;
+
+                object fieldValue = fieldInfo.GetValue(obj);
+
+
+
+                sb.AppendFormat($"{fieldName}:{fieldValue}");
+
+                sb.AppendLine();
+
+            }
+
+        }
+
+
+
+        return sb.ToString();
+    }
+}
