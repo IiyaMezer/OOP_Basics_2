1f78fd55122af99a42c22ea549eab9e72c69b2e9
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-12-01 23:30:11 +0700

    добавил аргументы

--- FamilyTree/HW_9.2/Program.cs
+++ FamilyTree/HW_9.2/Program.cs
@@ -7,8 +7,11 @@ internal class Program
     static void Main(string[] args)
     {
         JsonXmlConverter converter = new();
-        string jsonFilePath = "C:\\Users\\carna\\Source\\Repos\\IiyaMezer\\OOP_Basics_2\\FamilyTree\\HW_9.2\\generated.json";
-        string outputXml = "C:\\Users\\carna\\Source\\Repos\\IiyaMezer\\OOP_Basics_2\\FamilyTree\\HW_9.2\\output.xml";
+        //string jsonFilePath = "C:\\Users\\carna\\Source\\Repos\\IiyaMezer\\OOP_Basics_2\\FamilyTree\\HW_9.2\\generated.json";
+        //string outputXml = "C:\\Users\\carna\\Source\\Repos\\IiyaMezer\\OOP_Basics_2\\FamilyTree\\HW_9.2\\output.xml";
+
+        string jsonFilePath = args[0];
+        string outputXml = args[1];
 
         converter.ConvertJsonToXml(jsonFilePath, outputXml);
     }
